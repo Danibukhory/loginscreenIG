c453e481598779ea3a26bf4de2d9b17d06985677
danibukhory <dhanybukhory04@gmail.com>
2022-05-11 22:01:11 +0800

    Initial Commit

---
+++ loginscreen/loginscreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  loginscreenApp.swift
+//  loginscreen
+//
+//  Created by Muhammad dhani Bukhory on 11/05/22.
+//
+
+import SwiftUI
+
+@main
+struct loginscreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
